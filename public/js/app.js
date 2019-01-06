1989b0d773f23523c2c2db987920f0bcb6281fcf
ping.zhang <ping@pingzhangdeMacBook-Pro.local>
2019-01-06 23:37:14 +0800

    m facepig

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+function chatQQ(){
+  window.location.href="mqqwpa://im/chat?chat_type=wpa&uin=969936036&version=1&src_type=web&web_src=oicqzone.com";
+}
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  //res.sendFile(__dirname + '/pages/index.html');
+  res.sendfile('public/pages/index.html');
+});
+
+router.get('/product', function (req, res, next) {
+  res.sendfile('public/pages/product.html');
+});
+
+router.get('/download', function (req, res, next) {
+  res.sendfile('public/pages/download.html');
+});
+
+router.get('/buy', function (req, res, next) {
+  res.sendfile('public/pages/buy.html');
+});
+
+router.get('/course', function (req, res, next) {
+  res.sendfile('public/pages/course.html');
+});
+
+router.get('/test', function (req, res, next) {
+  res.render('test', {
+    title: '测试内容'
+  });
+});
+
+module.exports = router;
